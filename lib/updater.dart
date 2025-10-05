46e14977ccd655bfdf6d167ba47a263813c93823
Ds32 <dani192432@gmail.com>
2025-10-04 22:22:00 -0500

    Probando Git Assistant

--- lib/updater.dart
+++ lib/updater.dart
@@ -3,9 +3,9 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-import 'package:open_file_plus/open_file_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:open_file/open_file.dart';
 
 class Updater {
   final String githubUser;
